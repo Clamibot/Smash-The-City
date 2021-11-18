e83ecec9c98b45ae590bf9e79c1933c44dae366e
Christopher Hahn <comet.chris@sbcglobal.net>
2021-11-17 19:23:38 -0600

    11/17 Finished out the player vr actions
     - There seems to be problems with the quest and openvr/unity2020 or whatever

--- Assets/Scripts/Enemy and Player/PlayerVRActions.cs
+++ Assets/Scripts/Enemy and Player/PlayerVRActions.cs
@@ -5,9 +5,12 @@ using Valve.VR;
 
 public class PlayerVRActions : MonoBehaviour
 {
+    public float gravity = 30;
     public float sensitivity = 0.1f;
     public float maxSpeed = 1.0f;
+    public float rotateIncrement = 90;
 
+    public SteamVR_Action_Boolean rotatePress = null;
     public SteamVR_Action_Boolean movePressed = null;
     public SteamVR_Action_Vector2 moveValue = null;
 
@@ -15,8 +18,8 @@ public class PlayerVRActions : MonoBehaviour
     private Vector3 direction = Vector3.zero;
 
     private CharacterController charController;
-    private Transform cameraRig;
-    private Transform head;
+    public Transform cameraRig;
+    public Transform head;
 
     private void Awake()
     {
@@ -26,19 +29,20 @@ public class PlayerVRActions : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        cameraRig = SteamVR_Render.Top().origin;
-        head = SteamVR_Render.Top().head;
+        //cameraRig = SteamVR_Render.Top().origin;
+        //head = SteamVR_Render.Top().head;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        HandleHead();
+        //HandleHead()
         HandleLocalSpace();
         CalculateMovement();
+        SnapRotation();
     }
 
-    private void HandleHead()
+    /*private void HandleHead()
     {
         //Store current rotation
         Vector3 oldPos = cameraRig.position;
@@ -50,37 +54,7 @@ public class PlayerVRActions : MonoBehaviour
         //Restore
         cameraRig.position = oldPos;
         cameraRig.rotation = oldRot;
-    }
-
-    private void CalculateMovement()
-    {
-        //Figure out movement orientation
-        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
-        Quaternion orientation = Quaternion.Euler(orientationEuler);
-        Vector3 movement = Vector3.zero;
-
-        //If not moving
-        if (movePressed.GetStateUp(SteamVR_Input_Sources.Any))
-        {
-            speed = 0;
-            direction = Vector3.zero;
-        }
-
-        //If we are supposed to be moving
-        if (movePressed.state)
-        {
-            //Add, clamp
-            speed += moveValue.axis.magnitude * sensitivity;
-            speed = Mathf.Clamp(speed, -maxSpeed * 0.5f, maxSpeed);
-
-            //direction
-            direction = new Vector3(moveValue.axis.normalized.x, 0, moveValue.axis.normalized.y);
-            movement += orientation * (speed * direction) * Time.deltaTime;
-        }
-
-        //Apply
-        charController.Move(movement);
-    }
+    }*/
 
     private void HandleLocalSpace()
     {
@@ -97,10 +71,56 @@ public class PlayerVRActions : MonoBehaviour
         newCenter.x = head.localPosition.x;
         newCenter.z = head.localPosition.z;
 
-        //Rotate
-        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
-
         //Apply
         charController.center = newCenter;
+    }
+
+    private void CalculateMovement()
+    {
+        //Figure out movement orientation
+        Quaternion orientation = CalculateOrientation();
+        Vector3 movement = Vector3.zero;
+
+        //If not moving
+        if (moveValue.axis.magnitude == 0)
+        {
+            speed = 0;
+            direction = Vector3.zero;
+        }
+
+        //Add, clamp
+        speed += moveValue.axis.magnitude * sensitivity;
+        speed = Mathf.Clamp(speed, -maxSpeed * 0.5f, maxSpeed);
+
+        //direction & gravity
+        direction = new Vector3(moveValue.axis.normalized.x, 0, moveValue.axis.normalized.y);
+        movement += orientation * (speed * direction);
+        movement.y -= gravity * Time.deltaTime;
+
+        //Apply
+        Debug.Log(movement.x + " : " + moveValue.axis.magnitude + "\n" + rotatePress.state);
+        charController.Move(movement * Time.deltaTime);
+    }
+
+    private Quaternion CalculateOrientation()
+    {
+        float rotation = Mathf.Atan2(moveValue.axis.x, moveValue.axis.y);
+        rotation *= Mathf.Rad2Deg;
+
+        Vector3 orientationEuler = new Vector3(0, head.eulerAngles.y + rotation, 0);
+        return Quaternion.Euler(orientationEuler);
+    }
+
+    private void SnapRotation()
+    {
+        float snapValue = 0;
+
+        if (rotatePress.GetStateDown(SteamVR_Input_Sources.LeftHand))
+            snapValue = -Mathf.Abs(rotateIncrement);
+
+        if (rotatePress.GetStateDown(SteamVR_Input_Sources.RightHand))
+            snapValue = Mathf.Abs(rotateIncrement);
+
+        transform.RotateAround(head.position, Vector3.up, snapValue);
     }
 }
